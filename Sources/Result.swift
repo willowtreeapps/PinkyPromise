f36790f8a56e3d3850b2e5e3700769646eea4fdb
Kevin Conner <connerk@gmail.com>
2016-03-24 22:09:48 -0400

    Simplify Result implementation using Swift's error throwing instead of switching.

--- Sources/Result.swift
+++ Sources/Result.swift
@@ -10,13 +10,13 @@ import Foundation
 
 enum Result<T> {
 
-    typealias SuccessType = T
+    typealias Value = T
 
     case Failure(ErrorType)
-    case Success(SuccessType)
+    case Success(Value)
 
     // Unwrap a success value or throw a failure value.
-    func value() throws -> SuccessType {
+    func value() throws -> Value {
         switch self {
         case .Failure(let error):
             throw error
@@ -25,86 +25,55 @@ enum Result<T> {
         }
     }
 
-    func map<U>(@noescape transform: (SuccessType) throws -> U) -> Result<U> {
-        switch self {
-        case .Failure(let error):
-            return .Failure(error)
-        case .Success(let value):
-            do {
-                let transformedValue = try transform(value)
-                return .Success(transformedValue)
-            } catch {
-                return .Failure(error)
-            }
+    // Keep our error if we have one. Otherwise, transform the success value.
+    // Errors thrown during transformation are captured as failure values.
+    func map<U>(@noescape transform: (Value) throws -> U) -> Result<U> {
+        return flatMap { value in
+            let mappedValue = try transform(value)
+            return .Success(mappedValue)
         }
     }
 
-    func flatMap<U>(@noescape transform: (SuccessType) throws -> Result<U>) -> Result<U> {
-        switch self {
-        case .Failure(let error):
+    // Keep our error if we have one. Otherwise, produce a new success or failure using the success value.
+    // Errors thrown during transformation are captured as failure values.
+    func flatMap<U>(@noescape transform: (Value) throws -> Result<U>) -> Result<U> {
+        do {
+            let successValue = try value()
+            let mappedResult = try transform(successValue)
+            return mappedResult
+        } catch {
             return .Failure(error)
-        case .Success(let value):
-            do {
-                return try transform(value)
-            } catch {
-                return .Failure(error)
-            }
         }
     }
 
 }
 
 func zip<A, B>(resultA: Result<A>, _ resultB: Result<B>) -> Result<(A, B)> {
-    switch resultA {
-    case .Failure(let error):
-        return .Failure(error)
-    case .Success(let a):
-        switch resultB {
-        case .Failure(let error):
-            return .Failure(error)
-        case .Success(let b):
-            return .Success(a, b)
-        }
+    return resultA.flatMap { a in
+        let b = try resultB.value()
+        return .Success(a, b)
     }
 }
 
 func zip<A, B, C>(resultA: Result<A>, _ resultB: Result<B>, _ resultC: Result<C>) -> Result<(A, B, C)> {
-    switch resultA {
-    case .Failure(let error):
-        return .Failure(error)
-    case .Success(let a):
-        switch zip(resultB, resultC) {
-        case .Failure(let error):
-            return .Failure(error)
-        case .Success(let b, let c):
-            return .Success(a, b, c)
-        }
+    return resultA.flatMap { a in
+        let (b, c) = try zip(resultB, resultC).value()
+        return .Success(a, b, c)
     }
 }
 
 func zip<A, B, C, D>(resultA: Result<A>, _ resultB: Result<B>, _ resultC: Result<C>, _ resultD: Result<D>) -> Result<(A, B, C, D)> {
-    switch resultA {
-    case .Failure(let error):
-        return .Failure(error)
-    case .Success(let a):
-        switch zip(resultB, resultC, resultD) {
-        case .Failure(let error):
-            return .Failure(error)
-        case .Success(let b, let c, let d):
-            return .Success(a, b, c, d)
-        }
+    return resultA.flatMap { a in
+        let (b, c, d) = try zip(resultB, resultC, resultD).value()
+        return .Success(a, b, c, d)
     }
 }
 
 func zipArray<T>(results: [Result<T>]) -> Result<[T]> {
     return results.reduce(.Success([])) { (arrayResult, itemResult) in
         return arrayResult.flatMap { array in
-            switch itemResult {
-            case .Failure(let error):
-                return .Failure(error)
-            case .Success(let item):
-                return .Success(array + [item])
-            }
+            let item = try itemResult.value()
+            return .Success(array + [item])
         }
     }
 }
