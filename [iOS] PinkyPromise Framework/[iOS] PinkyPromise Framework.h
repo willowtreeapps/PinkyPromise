30c4b4ce8c0f9a9e2e1c9233a8a7580c25211636
Raf <raf.cabezas@willowtreeapps.com>
2017-03-28 10:52:42 -0400

    Framework support for carthage

---
+++ [iOS] PinkyPromise Framework/[iOS] PinkyPromise Framework.h
@@ -0,0 +1,19 @@
+//
+//  [iOS] PinkyPromise Framework.h
+//  [iOS] PinkyPromise Framework
+//
+//  Created by Raf Cabezas on 3/28/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for [iOS] PinkyPromise Framework.
+FOUNDATION_EXPORT double _iOS__PinkyPromise_FrameworkVersionNumber;
+
+//! Project version string for [iOS] PinkyPromise Framework.
+FOUNDATION_EXPORT const unsigned char _iOS__PinkyPromise_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <_iOS__PinkyPromise_Framework/PublicHeader.h>
+
+
