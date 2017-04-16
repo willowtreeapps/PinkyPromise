56735e60a186f4a6724ec5bdf943bab53d86fa91
Kevin Conner <connerk@gmail.com>
2017-04-16 18:09:53 -0400

    Support framework targets on iOS and macOS
    Add license to framework umbrella header
    Rename "[iOS] PinkyPromise Framework" to "PinkyPromise_iOS"
    Create PinkyPromiseTests_iOS target
    Use iOS framework tests for fastlane/slather/coveralls/CircleCI
    Add macOS framework target and test target
    Remove static library target and test target
    Rearrange navigator groups

---
+++ PinkyPromise_macOS/PinkyPromise_macOS.h
@@ -0,0 +1,19 @@
+//
+//  PinkyPromise_macOS.h
+//  PinkyPromise_macOS
+//
+//  Created by Kevin Conner on 4/16/17.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PinkyPromise_macOS.
+FOUNDATION_EXPORT double PinkyPromise_macOSVersionNumber;
+
+//! Project version string for PinkyPromise_macOS.
+FOUNDATION_EXPORT const unsigned char PinkyPromise_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PinkyPromise_macOS/PublicHeader.h>
+
+
