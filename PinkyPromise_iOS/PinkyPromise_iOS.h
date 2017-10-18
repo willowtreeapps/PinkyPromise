114f0f2a7b1c7ba629c53b6cea724e5c7670f4c2
Kevin Conner <connerk@gmail.com>
2017-10-18 10:40:19 -0400

    Fix some incorrect license comments

--- PinkyPromise_iOS/PinkyPromise_iOS.h
+++ PinkyPromise_iOS/PinkyPromise_iOS.h
@@ -5,7 +5,7 @@
 //  Created by Raf Cabezas on 3/28/17.
 //
 //  The MIT License (MIT)
-//  Copyright © 2016 WillowTree, Inc. All rights reserved.
+//  Copyright © 2017 WillowTree, Inc. All rights reserved.
 // 
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to
