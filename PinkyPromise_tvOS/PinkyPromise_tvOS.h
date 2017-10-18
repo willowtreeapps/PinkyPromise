a3123f52a895b3e2ef750cb41eed35d41670fe59
ecompton3 <ecomton3@gmail.com>
2017-10-18 10:05:12 -0400

    added tvOS support, rolled back macOS required version
    ignore Carthage files

---
+++ PinkyPromise_tvOS/PinkyPromise_tvOS.h
@@ -0,0 +1,18 @@
+//
+//  PinkyPromise_tvOS.h
+//  PinkyPromise_tvOS
+//
+//  Created by Evan Compton on 10/18/17.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PinkyPromise_tvOS.
+FOUNDATION_EXPORT double PinkyPromise_tvOSVersionNumber;
+
+//! Project version string for PinkyPromise_tvOS.
+FOUNDATION_EXPORT const unsigned char PinkyPromise_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PinkyPromise_tvOS/PublicHeader.h>
+
+
